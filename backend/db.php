6ffc2ad6920212a3fba2729b718ceb88187fce2e
jubairsiyum <jubairsiyum@gmail.com>
2024-12-13 19:16:57 +0600

    fg

--- backend/db.php
+++ backend/db.php
@@ -2,11 +2,24 @@
 $host = "localhost";
 $user = "root";
 $password = "";
-$database = "BloggingSystem";
+$database = "bloggingsystem";
 
 $conn = new mysqli($host, $user, $password, $database);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+$sql = "SELECT id, username, password, created_at FROM users";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "ID: " . $row["id"]. " - Username: " . $row["username"]. " - Password: " . $row["password"]. " - Created At: " . $row["created_at"];
+    }
+} else {
+    echo "0 results found.";
+}
+
+$conn->close();
 ?>
